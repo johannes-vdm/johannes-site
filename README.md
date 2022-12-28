fce8f3fd1f813bc1ff77fc3592bee3815e31368b
Johannes van der Merwe <johannes.vandermerwe@adaptit.com>
2022-12-28 13:06:39 +0200

    new icons, custom command

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 [![Netlify Status](https://api.netlify.com/api/v1/badges/696a7e28-cf6f-4ee9-92e5-2d26c493ef1f/deploy-status)](https://app.netlify.com/sites/johannesmerwe/deploys)
+
+```js
+// Generate PDF:
+window.generateResume();
+```
--- assets/js/main.js
+++ assets/js/main.js
@@ -98,33 +98,58 @@ function removeScale() {
 }
 
 /*==================== GENERATE PDF ====================*/ 
-// PDF generated area
-let areaCv = document.getElementById('area-cv');
+// // PDF generated area
+// let areaCv = document.getElementById('area-cv');
 
-let resumeButton = document.getElementById('resume-button');
+// let resumeButton = document.getElementById('resume-button');
 
-// Html2pdf options
-let opt = {
-    margin:       0,
-    filename:     'Johannes van der Merwe - CV.pdf',
-    image:        { type: 'jpeg', quality: 0.98 },
-    html2canvas:  { scale: 4 },
-    jsPDF:        { orientation: 'portrait' }
-};
+// // Html2pdf options
+// let opt = {
+//     margin:       0,
+//     filename:     'Johannes van der Merwe - CV.pdf',
+//     image:        { type: 'jpeg', quality: 0.98 },
+//     html2canvas:  { scale: 4 },
+//     jsPDF:        { orientation: 'portrait' }
+// };
 
-// Function to call areaCv and Html2Pdf options 
-function generateResume() {
-    html2pdf(areaCv, opt);
-}
+// // Function to call areaCv and Html2Pdf options 
+// function generateResume() {
+//     html2pdf(areaCv, opt);
+// }
 
-// When the button is clicked, it executes the three functions
-resumeButton.addEventListener('click', () => {
+// // When the button is clicked, it executes the three functions
+// resumeButton.addEventListener('click', () => {
+//     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+//     scaleCv();
+
+//     // 2. The PDF is generated
+//     generateResume();
+
+//     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+//     setTimeout(removeScale, 5000);
+// })
+
+window.generateResume = function() {
+    let areaCv = document.getElementById('area-cv');
+  
+    // Html2pdf options
+    let opt = {
+        margin:       0,
+        filename:     'Johannes van der Merwe - CV.pdf',
+        image:        { type: 'jpeg', quality: 0.98 },
+        html2canvas:  { scale: 4 },
+        jsPDF:        { orientation: 'portrait' }
+    };
+  
+    // Function to call areaCv and Html2Pdf options 
+    
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
     scaleCv();
-
+    
     // 2. The PDF is generated
-    generateResume();
-
+    html2pdf(areaCv, opt);
+  
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
     setTimeout(removeScale, 5000);
-})
+  }
+  
